2c227e2a37e6aa476a3a4b2ea19c1cc329d0646c
munna472 <mdmunnabiswas69@gmail.com>
2026-06-27 19:31:06 +0600

    Update rate.js

--- handlers/rate.js
+++ handlers/rate.js
@@ -1,1 +1,14 @@
+const { sendToGroup } = require("../utils/sender");
 
+module.exports = async (bot, msg) => {
+
+    const chatId = msg.chat.id;
+
+    await sendToGroup(bot, "Arate");
+
+    await bot.sendMessage(
+        chatId,
+        "✅ Rate command sent successfully."
+    );
+
+};
