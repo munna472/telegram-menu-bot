403bbb9cf06e5f2af101d395a96441eccb404281
munna472 <mdmunnabiswas69@gmail.com>
2026-06-27 19:21:43 +0600

    Add sendToGroup function to send messages

--- utils/sender.js
+++ utils/sender.js
@@ -1,1 +1,24 @@
+const { GROUP_ID } = require("../config/config");
 
+async function sendToGroup(bot, command) {
+    try {
+        await bot.sendMessage(GROUP_ID, command);
+
+        return {
+            success: true
+        };
+
+    } catch (err) {
+
+        console.log(err);
+
+        return {
+            success: false
+        };
+
+    }
+}
+
+module.exports = {
+    sendToGroup
+};
