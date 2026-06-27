dc12bf748008f52e53faf128d0f90220784ed891
munna472 <mdmunnabiswas69@gmail.com>
2026-06-27 19:22:54 +0600

    Update profile.js

--- handlers/profile.js
+++ handlers/profile.js
@@ -1,1 +1,14 @@
+const { sendToGroup } = require("../utils/sender");
 
+module.exports = async (bot, msg) => {
+
+    const chatId = msg.chat.id;
+
+    await sendToGroup(bot, "Aprofile");
+
+    await bot.sendMessage(
+        chatId,
+        "✅ Profile command sent successfully."
+    );
+
+};
