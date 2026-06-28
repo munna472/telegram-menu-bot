557cd9375ac337f5ea2ce62a2536f78f5779aadc
munna472 <mdmunnabiswas69@gmail.com>
2026-06-28 15:13:28 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-    BOT_TOKEN: process.env.BOT_TOKEN,
-    GROUP_ID: process.env.GROUP_ID,
+    BOT_TOKEN: process.env.BOT_TOKEN || "8908339374:AAGDZJtaRLQpF5lYgRkK2TKNtGztCEfU8AI",
+    GROUP_ID: process.env.GROUP_ID || "-1004413191032",
     PORT: process.env.PORT || 10000
 };
